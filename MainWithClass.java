f6d303beab0710694308f665b8255ee5dabc9a24
Rajeev Ranjan Prasad <rajeevranjan125@users.noreply.github.com>
2026-07-19 03:29:56 +0530

    learned multithreading

---
+++ MainWithClass.java
@@ -0,0 +1,19 @@
+package Multithreading;
+
+public class MainWithClass {
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread();
+
+        t1.start(); //new thread created and that new thread internally called run method
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+class MyThread extends Thread{
+
+    public void run(){
+       System.out.println(Thread.currentThread().getName());
+    }
+}
---
+++ MainWithInterface.java
@@ -0,0 +1,17 @@
+package Multithreading;
+
+/**
+ * InnerMainWithInterface
+ */
+class InnerMainWithInterface {
+
+
+}
+
+public class MainWithInterface {
+    
+    public static void main(String[] args) {
+
+        
+    }
+}
