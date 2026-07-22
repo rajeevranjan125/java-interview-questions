7974b0f185b7e42f9874e37c562d74336b8a018f
Rajeev Ranjan Prasad <rajeevranjan125@users.noreply.github.com>
2026-07-22 22:02:40 +0530

    learned runnable interface

---
+++ ThreadLifeCycle.java
@@ -0,0 +1,43 @@
+package Multithreading;
+
+public class ThreadLifeCycle {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Worker worker = new Worker();
+
+        System.out.println("After creating thread.." + worker.currentThread().getName());
+
+        worker.start();
+
+        Thread.sleep(200);
+
+        System.out.println("After start(): " + worker.currentThread().getName());
+
+        worker.join();
+
+        System.out.println("After join(): " + worker.currentThread().getName());
+        System.out.println("After join(): " + worker.currentThread().getName());
+        System.out.println("After join(): " + worker.currentThread().getName());
+        System.out.println("After join(): " + worker.currentThread().getName());
+
+    }
+
+}
+
+class Worker extends Thread {
+
+    @Override
+    public void run() {
+        try {
+
+            System.out.println("1.worker stated..");
+            System.out.println("2.worker sleep for 2 seconds..");
+            Thread.sleep(2000);
+            System.out.println("3.Woerker finished");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
